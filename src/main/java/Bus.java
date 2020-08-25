d29621de3b66a80e0f4cf29e46520db4d0890ade
peaky76 <robertjamespeacock@gmail.com>
2020-08-25 13:41:32 +0100

    Bus set up and busStartsEmpty test passed

---
+++ src/main/java/Bus.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Bus {
+
+    private String destination;
+    private int capacity;
+    private ArrayList<Person> passengers;
+
+    public Bus(String destination, int capacity) {
+        this.destination = destination;
+        this.capacity = capacity;
+        this.passengers = new ArrayList<Person>();
+    }
+
+    public int getNumberOfPassengers() {
+        return this.passengers.size();
+    }
+}
---
+++ src/test/java/BusTest.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BusTest {
+
+    private Bus bus;
+    private Person passenger;
+
+    @Before
+    public void before() {
+        bus = new Bus("Ocean Terminal", 80);
+        passenger = new Person();
+    }
+
+    @Test
+    public void busStartsEmpty() {
+        assertEquals(0, bus.getNumberOfPassengers());
+    }
+
+
+}
